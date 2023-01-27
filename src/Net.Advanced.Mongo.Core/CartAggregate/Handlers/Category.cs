b129f396914f71eae22a6fffe61de45eedbbe0d6
Nikolai Smirnov <nikolai_smirnov1@epam.com>
2023-01-27 22:20:01 +0400

    Message Based Architecture. Message Broker: consumer (#2)

---
+++ src/Net.Advanced.Mongo.Core/CartAggregate/Handlers/Category.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.GuardClauses;
+using Net.Advanced.Mongo.SharedKernel;
+
+namespace Net.Advanced.Mongo.Core.CartAggregate.Handlers;
+public class Category : EntityBase
+{
+  public Category(string name)
+  {
+    Name = Guard.Against.NullOrEmpty(name, nameof(name));
+  }
+
+  public string Name { get; set; }
+  public string? Image { get; set; }
+  public Category? Parent { get; set; }
+}
---
+++ src/Net.Advanced.Mongo.SharedKernel/EntityChangedEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace Net.Advanced.Mongo.SharedKernel;
+
+public class EntityChangedEvent<T> : DomainEventBase
+  where T : EntityBase
+{
+  private readonly List<string> _changedProps = new();
+
+  public EntityChangedEvent(T entity, params string[] changedProps)
+  {
+    Entity = entity;
+    _changedProps.AddRange(changedProps);
+  }
+
+  public T Entity { get; set; }
+
+  public string EntityType => typeof(T).Name;
+
+  public IReadOnlyCollection<string> ChangedProps => _changedProps.AsReadOnly();
+}
